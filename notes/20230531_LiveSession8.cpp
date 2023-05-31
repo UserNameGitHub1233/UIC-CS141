8816612f1ea732a38f2f70ca138d716b5992c630
UserNameGitHub1233 <135139729+UserNameGitHub1233@users.noreply.github.com>
2023-05-31 09:26:40 -0500

    Update 20230531_LiveSession8.cpp

--- notes/20230531_LiveSession8.cpp
+++ notes/20230531_LiveSession8.cpp
@@ -9,7 +9,7 @@
 ///         files (a .h header and a .cpp implementation file).
 
 
-#include <iostream> hello
+#include <iostream>
 #include <fstream>
 #include <algorithm>    // for sort()
 #include <sstream>      // for stringstream conversions
